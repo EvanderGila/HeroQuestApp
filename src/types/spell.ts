8e11afbd02ea5c0d1ca0cf5f58af2f4e95dfa50d
EvanderGila <evander.gila.reques@gmail.com>
2026-06-22 18:22:48 +0200

    type(frontend): Add classes to spell.

--- src/types/spell.ts
+++ src/types/spell.ts
@@ -4,9 +4,14 @@ export interface Spell {
   img: string | null
   mana_cost: number
   description: string
+  class_spells:ClassSpell[]
 }
 
 export interface ClassSpell {
   required_lvl: number
   spells: Spell
+  classes?: { 
+    id: number
+    name: string
+  } | null
 }
